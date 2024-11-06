5a2b8921dedc12fff7a8a514d4f4c560a8953e77
mohithmandya <mohithrgowda2004@gmail.com>
2024-11-06 13:10:52 +0530

    nov6-2

---
+++ NOV5/divby3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number");
+    scanf("%d",n);
+    if((n%3)==0){
+        printf("true");
+
+    }
+
+}
---
+++ NOV5/nestedif.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a=14,b=5,c=6;
+    if(a>b){
+        if(a>c){
+            printf("true\n");
+        }
+        else{
+
+            printf("invalid");            
+        }
+    }
+}
